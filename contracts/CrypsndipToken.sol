e57abba5aefd5057387b3786adf22259bd073cf0
Chris <chrisliu0416@gmail.com>
2020-03-25 18:59:33 -0700

    3. Delegated Transfer

--- contracts/CrypsndipToken.sol
+++ contracts/CrypsndipToken.sol
@@ -12,7 +12,15 @@ contract CrypsndipToken {
         uint256 _value
     );
 
+    // approve
+    event Approval (
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -20,7 +28,6 @@ contract CrypsndipToken {
         // allocate the initial supply
     }
 
-    // Transfer
     function transfer(address _to, uint256 _value) public returns (bool success) {
 
         require(balanceOf[msg.sender] >= _value);
@@ -32,4 +39,29 @@ contract CrypsndipToken {
 
         return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+
 }
